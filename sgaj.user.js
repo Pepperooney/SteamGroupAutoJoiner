5c95d8a6eca45cc843da3eb7e8a2eb1883c8055c
Eldar <the.rooney.10@gmail.com>
2016-03-28 17:36:18 +0600

    Added files via upload

---
+++ sgaj.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name			SteamGroupAutoJoiner
+// @namespace		SteamGroupAutoJoiner
+// @description		Autojoins steam groups
+// @version			0.1
+// @author			Pepperooney
+// @include			http://steamcommunity.com/groups/*
+// @supportURL   	https://github.com/Pepperooney/SteamGroupAutoJoiner/issues
+// @updateURL    	https://github.com/Pepperooney/SteamGroupAutoJoiner/raw/master/autojoin.user.js
+// @downloadURL  	https://github.com/Pepperooney/SteamGroupAutoJoiner/raw/master/autojoin.user.js
+// @grant 			window.close
+// ==/UserScript==
+var content = document.body.textContent || document.body.innerText;
+var hasText = content.indexOf("Join Group")!==-1;
+if(hasText){
+    javascript:document.forms['join_group_form'].submit();
+}
+//to do
+var hasTextru = content.indexOf("Присоединиться")!==-1; 
+if(hasTextru){
+    javascript:document.forms['join_group_form'].submit();
+}
+setTimeout (window.close, 5000);
